240d5c07d5133144bb19ffeb297acd005400a80d
PriyanshuDigari-code <ds0053395@gmail.com>
2026-09-08 22:33:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://multiple-pdf-summarization-system-4qzl.onrender.com";
 
 const pdfFiles = document.getElementById("pdfFiles");
 const fileList = document.getElementById("fileList");
